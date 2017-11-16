09de5f03c1260f80417b6021f762165626671661
insekkei <insekkei@yahoo.com>
2017-11-16 20:51:31 +0800

    ..

--- daipu/category-press.php
+++ daipu/category-press.php
@@ -26,7 +26,9 @@ get_header('daipu'); ?>
                 $sortArgs = array(
                     'orderby' => 'date'
                 );
-                $args = array_merge( $wp_query->query_vars,  $sortArgs);
+                $query_vars = $wp_query->query_vars;
+                $query_vars['posts_per_page'] = -1
+                $args = array_merge( $query_vars,  $sortArgs );
 
                 query_posts($args);
 
@@ -73,11 +75,7 @@ get_header('daipu'); ?>
 			// End the loop.
 			endwhile;
 			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-			) );
+
 
 		// If no content, include the "No posts found" template.
 		else :
